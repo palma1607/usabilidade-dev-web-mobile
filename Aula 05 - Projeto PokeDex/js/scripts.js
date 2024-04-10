76113580e8083f9128b780d95bceaf28885e0648
palma1607 <palma1607@gmail.com>
2024-04-09 21:59:02 -0300

    Add files via upload

--- Aula 05 - Projeto PokeDex/js/scripts.js
+++ Aula 05 - Projeto PokeDex/js/scripts.js
@@ -1,14 +1,15 @@
 
 
+const grupoGrama = ["bulbasaur","venusaur","ivysaur"]
+const grupoFogo = ["charmander","charmeleon","charizard"]
+
 
 function carregarPokemons(){
-
-
 
     const requisicaoHttp = new XMLHttpRequest()
     requisicaoHttp.open("GET","https://pokeapi.co/api/v2/pokemon",false)
     requisicaoHttp.send()
-
+    
     const resposta = JSON.parse(requisicaoHttp.responseText)
     const pokemons = resposta.results
 
@@ -20,17 +21,29 @@ function carregarPokemons(){
     }
 }
 
-
 function adicionarCardPokemon(pokemon){
+
+    const divCardElemento = document.createElement("div")
+    divCardElemento.classList.add("card")
 
     const imagemElemento = document.createElement("img")
     imagemElemento.setAttribute("src", pokemon.imagem)
+    imagemElemento.style.width="90%"
 
     const textoElemento = document.createElement("h1")
     textoElemento.innerHTML = pokemon.name
 
+    if(grupoGrama.includes(pokemon.name)){
+        textoElemento.classList.add("grama")
+    } else if(grupoFogo.includes(pokemon.name)){
+        textoElemento.classList.add("fogo")
+    }
+   
     const divElemento = document.getElementById("conteudo-pokemon")
-    divElemento.appendChild(imagemElemento)
-    divElemento.appendChild(textoElemento)
+
+    divCardElemento.appendChild(imagemElemento)
+    divCardElemento.appendChild(textoElemento)
+
+    divElemento.appendChild(divCardElemento)
 
 }
